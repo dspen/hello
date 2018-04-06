01669572cf39e61a9121ee57b2dc2c09e9953339
dspen <spencer.darylt@gmail.com>
2018-04-06 08:19:21 -0600

    webscrape example

---
+++ example_webscrape.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 22:58:12 2018
+
+@author: Daryl
+"""
+
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns true if the response seems to be HTML, false otherwise
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors. 
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+    
+response = simple_get('https://www.tradingview.com/cryptocurrency-signals/')
+soup = BeautifulSoup(response,'html5lib')
+print(soup.prettify())
+print(soup.get_text())
+for link in soup.find_all('a'):
+    print(link.get('href'))
